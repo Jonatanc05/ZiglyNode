b79e5bf804d2be8e1692cba8ec4050f363c64276
Jonatanc05 <jonatanc0511@gmail.com>
2026-01-02 17:49:19 -0300

    feat: building WebAssembly

--- build.zig
+++ build.zig
@@ -4,67 +4,116 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "ZiglyNode",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
-        .use_llvm = true,
-    });
-    exe.addIncludePath(b.path("include"));
-
-    const sdl3 = b.dependency("sdl3", .{
-        .target = target,
-        .optimize = optimize,
-        .c_sdl_preferred_linkage = .static,
-    });
-    exe.root_module.addImport("sdl3", sdl3.module("sdl3"));
-
-    const dvui_dep = b.dependency("dvui", .{ .target = target, .optimize = optimize, .backend = .sdl3 });
-    exe.root_module.addImport("dvui", dvui_dep.module("dvui_sdl3"));
-
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    // run from installation directory rather than directly from cache directory
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // allows `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // ------------ TESTS ------------
-
-    const test_step = b.step("test", "Run unit tests");
-
-    const files = [_][]const u8{
-        "src/util.zig",
-        "src/elliptic-curve.zig",
-        "src/cryptography.zig",
-        "src/bitcoin.zig",
-        "src/network.zig",
-    };
-
-    for (files) |file| {
-        const t = b.addTest(.{
-            //.name = file,
+    // Native SDL3 executable
+    {
+        const exe = b.addExecutable(.{
+            .name = "ZiglyNode",
             .root_module = b.createModule(.{
-                .root_source_file = b.path(file),
+                .root_source_file = b.path("src/main.zig"),
                 .target = target,
                 .optimize = optimize,
             }),
             .use_llvm = true,
         });
-        t.root_module.addIncludePath(b.path("include"));
-        const test_artifact = b.addRunArtifact(t);
-        test_step.dependOn(&test_artifact.step);
+        exe.addIncludePath(b.path("include"));
+
+        const sdl3 = b.dependency("sdl3", .{
+            .target = target,
+            .optimize = optimize,
+            .c_sdl_preferred_linkage = .static,
+        });
+        exe.root_module.addImport("sdl3", sdl3.module("sdl3"));
+
+        const dvui_dep = b.dependency("dvui", .{ .target = target, .optimize = optimize, .backend = .sdl3 });
+        exe.root_module.addImport("dvui", dvui_dep.module("dvui_sdl3"));
+
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+
+        // run from installation directory rather than directly from cache directory
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        // allows `zig build run -- arg1 arg2 etc`
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        // This will evaluate the `run` step rather than the default, which is "install".
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+    }
+
+    // WASM build
+    {
+        const wasm_target = b.resolveTargetQuery(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        });
+
+        const dvui_web_dep = b.dependency("dvui", .{
+            .target = wasm_target,
+            .optimize = optimize,
+            .backend = .web
+        });
+
+        const wasm_exe = b.addExecutable(.{
+            .name = "web",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = wasm_target,
+                .optimize = optimize,
+                .link_libc = false,
+                .strip = if (optimize == .ReleaseFast or optimize == .ReleaseSmall) true else false,
+            }),
+        });
+        wasm_exe.entry = .disabled;
+        wasm_exe.addIncludePath(b.path("include"));
+        wasm_exe.root_module.addImport("dvui", dvui_web_dep.module("dvui_web"));
+
+        const install_dir: std.Build.InstallDir = .{ .custom = "bin/web" };
+
+        const install_wasm = b.addInstallArtifact(wasm_exe, .{
+            .dest_dir = .{ .override = install_dir },
+        });
+
+        // Install web support files from DVUI dependency
+        const dvui_web_src = b.dependency("dvui", .{});
+        const web_html = dvui_web_src.path("src/backends/index.html");
+        const web_js = dvui_web_src.path("src/backends/web.js");
+
+        const wasm_step = b.step("wasm", "Build for WebAssembly");
+        wasm_step.dependOn(&b.addInstallFileWithDir(web_html, install_dir, "index.html").step);
+        wasm_step.dependOn(&b.addInstallFileWithDir(web_js, install_dir, "web.js").step);
+        wasm_step.dependOn(&install_wasm.step);
+    }
+
+    // Tests
+    {
+        const test_step = b.step("test", "Run unit tests");
+
+        const files = [_][]const u8{
+            "src/util.zig",
+            "src/elliptic-curve.zig",
+            "src/cryptography.zig",
+            "src/bitcoin.zig",
+            "src/blockchain.zig",
+            "src/network.zig",
+        };
+
+        for (files) |file| {
+            const t = b.addTest(.{
+                //.name = file,
+                .root_module = b.createModule(.{
+                    .root_source_file = b.path(file),
+                    .target = target,
+                    .optimize = optimize,
+                }),
+                .use_llvm = true,
+            });
+            t.root_module.addIncludePath(b.path("include"));
+            const test_artifact = b.addRunArtifact(t);
+            test_step.dependOn(&test_artifact.step);
+        }
     }
 }
